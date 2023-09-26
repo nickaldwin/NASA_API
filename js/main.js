64926df586112b3cb312da25387515599b0b6aad
nickaldwin <nicklemoncito98@gmail.com>
2023-09-26 15:18:10 +0800

    adding api_key

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  //NASA api_key
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=2bazCt5nywonJnp8qokrWtL47eRY4rMgHiTlJWIG'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation 
       })
       .catch(err => {
           console.log(`error ${err}`)
